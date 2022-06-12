d99c5f5fb62cef12e340fb41a9c006d0d79d2235
Gopal Saini <gopsaini@WKMZTF96E65D.global.publicisgroupe.net>
2022-06-12 16:32:50 -0400

    visual regression testing

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,21 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { setDefaultOptions } from "jsdom-screenshot";
+
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
+
+expect.extend({ toMatchImageSnapshot });
+
+setDefaultOptions({
+  serve: ['src']
+});
+
+// to load global stylesheet.
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "/App.css";
+document.head.appendChild(styleLink);
+
+// to increase the timeout because of image snapshot.
+// jest.setTimeout(30000);
